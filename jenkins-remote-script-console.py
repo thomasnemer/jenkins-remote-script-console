c974ba2c6315fba49c1e47dde7e5b7009da18357
Thomas Nemer <thomas.nemer@fortytwo.fr>
2021-07-15 16:34:58 +0200

    feat: Add some exception handling

--- jenkins-remote-script-console.py
+++ jenkins-remote-script-console.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
-import os,argparse,requests
+import os,argparse,requests,sys
 from dotenv import load_dotenv
 from pathlib import Path
-requests.packages.urllib3.disable_warnings()
 
 # Parse args
 parser = argparse.ArgumentParser()
@@ -18,11 +17,35 @@ USER = os.getenv('JENKINS_USER')
 TOKEN = os.getenv('JENKINS_TOKEN')
 SSL_VERIFY = os.getenv('JENKINS_SSL_VERIFY', 'true').lower in ('true', '1')
 
+# If we don't want to check for ssl certificate validity, we don't want to be warned about it either
+if not SSL_VERIFY:
+  requests.packages.urllib3.disable_warnings()
+
 # Parse script file
 with open(args.script, 'r') as script_file:
   script = script_file.read()
 
 # Do the thing
 endpoint = INSTANCE + '/scriptText'
-r = requests.post(endpoint, auth=(USER, TOKEN), data={'script': script}, verify=SSL_VERIFY)
-print(r.text)
+try:
+  req = requests.post(endpoint, auth=(USER, TOKEN), data={'script': script}, verify=SSL_VERIFY)
+  req.raise_for_status()
+except requests.ConnectionError as e:
+  print(str(e), file=sys.stderr)
+  print("Check your network connection.", file=sys.stderr)
+  sys.exit(1)
+except requests.Timeout as e:
+  print(str(e), file= sys.stderr)
+  print("Check jenkins is up.", file=sys.stderr)
+  sys.exit(1)
+except KeyboardInterrupt:
+  print("User interruption.", file=sys.stderr)
+  sys.exit(1)
+except requests.exceptions.HTTPError as e:
+  print(str(e), file=sys.stderr)
+  sys.exit(1)
+except requests.RequestException as e:
+  print(str(e), file=sys.stderr)
+  sys.exit(1)
+
+print(req.text)
